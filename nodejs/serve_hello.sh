95657e726eddf0696a876ebbb72a5d08c7962894
Bartek Zdanowski <bzd@touk.pl>
2015-06-17 00:54:31 +0200

    added some description, script runs multiple nodejs instances

--- nodejs/serve_hello.sh
+++ nodejs/serve_hello.sh
@@ -1,1 +1,13 @@
-nodejs server-hello.js $1 
+if [ $# -ne 2 ]; then
+    echo usage:    
+    echo $0 base_port number_of_instances
+    exit 1
+fi
+
+for i in `seq 1 $2`; do
+    port=$(($1 + i))
+    nodejs server-hello.js $port &
+done  
+
+echo spanwned $i nodejs servers
+echo use killall nodejs to stop
